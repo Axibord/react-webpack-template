b7044c0698df4e2b335f5a4fbb2b1e9bfbedf456
Axibord <ghiles.aitlounis@gmail.com>
2020-08-17 23:04:56 +0100

    react webpack/babel/tailwindcss template optimized (splitchunks and caching)

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	purge: {
+		enabled: false, // turn it to false in developement to have all tailwind classes
+		content: ['./src/*.js', './src/*.jsx', './src/**/*.jsx', './src/**/*.js']
+	},
+	theme: {
+		extend: {}
+	},
+	variants: {},
+	plugins: []
+}
---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const path = require('path') // resolve path
+const HtmlWebpackPlugin = require('html-webpack-plugin') // create file.html
+const { CleanWebpackPlugin } = require('clean-webpack-plugin') // clean the folder each build
+const MiniCssExtractPlugin = require('mini-css-extract-plugin') // extract css to files
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin') // minify css
+const TerserPlugin = require('terser-webpack-plugin') // minify js
+
+module.exports = {
+	mode: 'development',
+	devtool: 'eval-cheap-source-map',
+	devServer: { contentBase: './prod', port: 3000, hot: true },
+
+	entry: {
+		index: './src/index.js'
+	},
+
+	output: {
+		filename: '[name].[hash].bundle.js', // for production use [contenthash], for developement use [hash]
+		path: path.resolve('./prod')
+	},
+	plugins: [
+		new MiniCssExtractPlugin({ filename: '[name].[contenthash].css', chunkFilename: '[id].[contenthash].css' }),
+		// new CleanWebpackPlugin(),
+		new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }), // tell webpack that we dont want to remove index.html after each update
+		new HtmlWebpackPlugin({
+			template: './index.html',
+			filename: 'index.html'
+		})
+	],
+
+	optimization: {
+		minimizer: [new TerserPlugin(), new OptimizeCssAssetsPlugin({})],
+
+		moduleIds: 'hashed',
+		runtimeChunk: 'single',
+		splitChunks: {
+			cacheGroups: {
+				vendor: {
+					test: /[\\/]node_modules[\\/]/,
+					name: 'vendors',
+					chunks: 'all'
+				}
+			}
+		}
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.(css|scss)$/,
+				use: [MiniCssExtractPlugin.loader, 'css-loader']
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/i,
+				use: ['file-loader']
+			},
+			{
+				test: /\.html$/,
+				use: [{ loader: 'html-loader' }]
+			},
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /(node_modules|bower_components|prod)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env', '@babel/preset-react']
+					}
+				}
+			}
+		]
+	}
+}
