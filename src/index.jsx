726328403b6631aa8772427e3f95908d6f43d950
Axibord <ghiles.aitlounis@gmail.com>
2020-09-11 07:07:37 +0100

    added Eslint and Prettier configs using Airbnb style

---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './output.css'
+
+const App = () => {
+	return (
+		<div className='flex flex-col justify-center items-center min-h-screen bg-gray-900'>
+			<h1 className='text-6xl text-green-400 border-b-4 pb-4'>Webpack compiled React successfully</h1>
+			<h2 className='text-2xl text-white mt-10'>
+				Read `npm run build` description in github to see how to optimize for production
+			</h2>
+		</div>
+	)
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
