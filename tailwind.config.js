cb1328c71c3c2240d0be6e99035015732939ff4c
Axibord <ghiles.aitlounis@gmail.com>
2020-12-25 21:03:25 +0100

    upgrade to tailwindV2 and updated README.MD

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,4 @@
 module.exports = {
-	future: {
-		removeDeprecatedGapUtilities: true,
-		purgeLayersByDefault: true
-	},
 	purge: {
 		enabled: false, // turn it to false in developement to have all tailwind classes
 		content: ['./src/*.js', './src/*.jsx', './src/**/*.jsx', './src/**/*.js']
